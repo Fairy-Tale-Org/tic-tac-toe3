caefad19d0a223f60c0ff3c396375c3ddd823cbf
Andrew Y <lucky.subject@gmail.com>
2023-02-02 22:07:13 +0000

    Initial Commit

---
+++ tic-tac-toe3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tic-tac-toe3
+//
+//  Created by Confetti on 2.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tic-tac-toe3UITests/tic_tac_toe3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  tic_tac_toe3UITestsLaunchTests.swift
+//  tic-tac-toe3UITests
+//
+//  Created by Confetti on 2.02.23.
+//
+
+import XCTest
+
+final class tic_tac_toe3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
